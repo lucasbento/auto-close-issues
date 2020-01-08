f344cae06a16cfb8e4de33d7eab1aefe1b4f1da1
Lucas Bento <lucas.bsilva@outlook.com>
2020-01-08 11:07:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Automatically close issues
+
+Automatically close issues that don't follow the issue template.
+
+## How it works
+
+This GitHub action will fetch all the files inside of `.github/ISSUE_TEMPLATE`, parse the titles and compare those titles with the content of the opened/edited issue, if they don't match, it will:
+
+1. Add a label to the issue (configurable with the input property `closed-issues-label`);
+   > This step won't run if no label is provided.
+1. Add a message to the issue;
+1. Close it.
+
+If the user happens to edit the issue to match the template, the action will run again and, if it matches, it will remove the label and reopen the issue automatically.
+
+> This will only work if you have the input property `closed-issues-label` provided.
+
+## Installation
+
+Put the following content in the file `.github/workflows/main.yml`:
+
+```yml
+on:
+  issues:
+    types: [opened, edited]
+
+jobs:
+  auto_close_issues:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v1
+      - name: Automatically close issues that don't follow the issue template
+        uses: lucasbento/auto-close-issues@v0.0.1
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          issue-close-message: "@${issue.user.login}: hello! :wave:\n\nThis issue is being automatically closed because it does not follow the issue template." # optional property
+          closed-issues-label: "🙁 Not following issue template" # optional property
+```
+
+## Configuration
+
+You can configure `issue-close-message` and `closed-issues-label`, which are, respectively, the message that is shown when closing the issue and the label added to the issue when it being closed.
+
+For `issue-close-message` the example configuration uses `issue.user.login` to mention the user's username, you can check what you can specify on the message on [GitHub webhook documentation](https://developer.github.com/v3/activity/events/types/#webhook-payload-example-15).
+
+## License
+
+This project is released under the MIT license.
---
+++ src/index.js
@@ -0,0 +1,111 @@
+const fs = require("fs");
+const core = require("@actions/core");
+const github = require("@actions/github");
+const mdjson = require("mdjson");
+
+const ISSUE_TEMPLATE_DIR = ".github/ISSUE_TEMPLATE";
+
+// Grab the closing message from params or fallback to a default message
+const getIssueCloseMessage = () => {
+  const message =
+    core.getInput("issue-close-message") ||
+    "@${issue.user.login}: hello! :wave:\n\nThis issue is being automatically closed because it does not follow the issue template.";
+
+  const { payload } = github.context;
+
+  return Function(
+    ...Object.keys(payload),
+    `return \`${message}\``
+  )(...Object.values(payload));
+};
+
+(async () => {
+  const client = new github.GitHub(
+    core.getInput("github-token", { required: true })
+  );
+
+  const { payload } = github.context;
+
+  const issueBodyMarkdown = payload.issue.body;
+  // Get all the markdown titles from the issue body
+  const issueBodyTitles = Object.keys(mdjson(issueBodyMarkdown));
+
+  // Get a list of the templates
+  const issueTemplates = fs.readdirSync(ISSUE_TEMPLATE_DIR);
+
+  // Compare template titles with issue body
+  const doesIssueMatchAnyTemplate = issueTemplates.some(template => {
+    const templateMarkdown = fs.readFileSync(
+      `${ISSUE_TEMPLATE_DIR}/${template}`,
+      "utf-8"
+    );
+    const templateTitles = Object.keys(mdjson(templateMarkdown));
+
+    return templateTitles.every(title => issueBodyTitles.includes(title));
+  });
+
+  const { issue } = github.context;
+  const closedIssueLabel = core.getInput("closed-issues-label");
+
+  if (doesIssueMatchAnyTemplate) {
+    // Only reopen the issue if there's a `closed-issues-label` so it knows if it was previously closed
+    // because of the wrong template
+    if (payload.issue.state === "closed" && closedIssueLabel) {
+      const labels = (
+        await client.issues.listLabelsOnIssue({
+          owner: issue.owner,
+          repo: issue.repo,
+          issue_number: issue.number
+        })
+      ).data.map(({ name }) => name);
+
+      if (!labels.includes(closedIssueLabel)) {
+        return;
+      }
+
+      await client.issues.removeLabel({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+        name: closedIssueLabel
+      });
+
+      await client.issues.update({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+        state: "open"
+      });
+
+      return;
+    }
+
+    return;
+  }
+
+  // If an closed issue label was provided, add it to the issue
+  if (closedIssueLabel) {
+    await client.issues.addLabels({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      labels: [closedIssueLabel]
+    });
+  }
+
+  // Add the issue closing comment
+  await client.issues.createComment({
+    owner: issue.owner,
+    repo: issue.repo,
+    issue_number: issue.number,
+    body: getIssueCloseMessage()
+  });
+
+  // Close the issue
+  await client.issues.update({
+    owner: issue.owner,
+    repo: issue.repo,
+    issue_number: issue.number,
+    state: "closed"
+  });
+})();
